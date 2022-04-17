8705093148b96cf53dc306d4bb4736e1fe36e863
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-04-17 16:14:29 +0900

    blackのコードフォーマットを反映

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import datetime
 
+
 def main():
-    NOW :datetime.datetime = datetime.datetime.now()
+    NOW: datetime.datetime = datetime.datetime.now()
     print(NOW)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
